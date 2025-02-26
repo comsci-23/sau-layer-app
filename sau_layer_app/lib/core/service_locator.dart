8b2fb3dac312b35f84b26a9135bcd2b86c8a79f4
chaiwat.dev <>
2025-02-27 00:52:17 +0700

    refactor : refactor use case name & add comment.

--- sau_layer_app/lib/core/service_locator.dart
+++ sau_layer_app/lib/core/service_locator.dart
@@ -1,6 +1,6 @@
 import 'package:get_it/get_it.dart';
-import 'package:sau_layer_app/presentation/nasa_history/bloc/nasa_history_bloc.dart';
-import 'package:sau_layer_data/domain/usecases/fetch_layer_data.dart';
+import 'package:sau_layer_app/presentation/nasa_history/bloc/nasa_history_bloc.dart' as bloc;
+import 'package:sau_layer_data/domain/usecases/fetch_layer_data.dart' as usecase;
 import 'package:sau_layer_data/data/datasources/nasa_layer_service.dart';
 import 'package:sau_layer_data/domain/repositories/nasa_layer_repository.dart';
 import 'package:sau_layer_data/data/repositories/nasa_layer_repository_impl.dart';
@@ -13,8 +13,10 @@ void setup() {
   getIt.registerLazySingleton<NasaLayerRepository>(
     () => NasaLayerRepositoryImpl(getIt()), 
   );
+  // register usecase
+  getIt.registerLazySingleton<usecase.NasaHistory>(() => usecase.NasaHistory(getIt()));
 
-  getIt.registerLazySingleton<FetchLayerData>(() => FetchLayerData(getIt()));
 
-  getIt.registerFactory(() => NasaHistoryBloc(fetchLayerData: getIt()));
+  // register bloc
+  getIt.registerFactory(() => bloc.NasaHistoryBloc(nasaHistory: getIt()));
 }
